52ff3c958104eaae25a2a8ff8a371e269ff625dc
Jason Botesazan <jbotesazan@gmail.com>
2022-01-28 10:33:55 -0500

    completed bootstrap

--- my-app/src/components/Store.js
+++ my-app/src/components/Store.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  Card, Container, Button } from 'react-bootstrap';
+import {  Card, Container, Button, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function Store ({ memeFTs, buyMeme }) {
@@ -14,17 +14,28 @@ function Store ({ memeFTs, buyMeme }) {
     }
 
     return(
-        <div className='App'>
-            <Container>
-                <h1 style={{color:'#000000'}}>Welcome to the Store</h1>
+        <Container>
+            <div className='App'>
+                <h1 style={{color:'#7FFFD4'}}>Welcome to the Store</h1>
+                <Row>
                 {memesForSale.map(random => 
-                    <Card key={random.id}>
-                        <img src={random.src}/>
-                        <button onClick={() => handleSold(random)}>Buy</button>
+                    <Card key={random.id} style={{ background: '#ADD8E6', width: "400px", height: "500px", padding: "10px", margin: "10px"}}>
+                        <div key={random.id}>
+                            <Card.Title style={{color: 'black'}}>{random.title}</Card.Title>
+                            <Card.Img src={random.src} style={{ width: "200px", height: "200px"}} alt={random.title}/>
+                                <Card style={{background:'#778899', color:'#7FFFD4'}} className = 'm-4' >
+                                    <Card.Text>Price: {random.price} 💎</Card.Text>
+                                    <Card.Text>Owner: {random.owner}</Card.Text>
+                                    <Card.Text>Creator: {random.creator}</Card.Text>
+                                    <Card.Footer>MFT ™️</Card.Footer>
+                                    <Button variant='secondary' className="button" type="submit" style={{background:'#FFD700', color: '#778899'}} onClick={() => handleSold(random)}>Buy</Button>
+                                </Card>
+                        </div>
                     </Card>       
                 )}
-            </Container>
-        </div>
+                </Row>
+            </div>
+        </Container>
     )
 }
 
